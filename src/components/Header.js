e8ab33971ffbbb8cc4bf686f943bce5ae0bed038
Ryan Pitts <ryanpitts@protonmail.com>
2018-10-17 20:42:40 -0300

    Added logo and create board card

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 const Header = ({props}) =>
 <nav className="navbar navbar-dark bg-dark">
-	<Link className="navbar-brand" to="/">clak</Link>
+	<Link className="navbar-brand" to="/"><img src="/claklogo.png" height="40" alt="clak"/></Link>
 	<form className="form-inline">
 		<input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
 	</form>
